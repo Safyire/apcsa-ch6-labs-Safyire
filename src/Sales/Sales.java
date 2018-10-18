29de5b14668950a2cb38c6372d6a2f66610dd7a9
Safyire <safyire.n8@gmail.com>
2018-10-18 08:49:12 -0700

    Created Ch6 Labs

--- src/Sales/Sales.java
+++ src/Sales/Sales.java
@@ -1,36 +1,33 @@
 package Sales;
 /**
- * Name:
- * Date:
- * Period:
+ * Name: Nathan Piedad
+ * Date: 10/18/2018
+ * Period: 1
  *
- * Sales.Sales.java
+ * Sales.java
  *
  * Reads in and stores sales for each of 5 salespeople.  Displays
  * sales entered by salesperson id and total sales for all salespeople.
  */
-import sun.jvm.hotspot.oops.Array;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Sales
-{
-    public static void main(String[] args)
-    {
-
+public class Sales {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        //TODO 4) Prompt the user for the number of sales people that they need to enter
-        //TODO 4) and modify the program to create an appropriately sized array.
-
-        
-        final int SALESPEOPLE = 5;
-        int[] sales = new int[SALESPEOPLE];
+        //Prompt the user for the number of sales people that they need to enter
+        //and modify the program to create an appropriately sized array.
+        System.out.print("Enter the number of salespeople: ");
+        int salesSize = scan.nextInt();
+        if (salesSize < 1) {
+            throw new IllegalStateException("You cannot have less than one salesperson!");
+        }
+        int[] sales = new int[salesSize];
         int sum;
 
-        for (int i=0; i < sales.length; i++)
-        {
+        for (int i=0; i < sales.length; i++) {
             System.out.print("Enter sales for salesperson " + (i + 1) + ": ");
             sales[i] = scan.nextInt();
         }
@@ -40,45 +37,62 @@ public class Sales
         System.out.println("\nSalesperson   Sales");
         System.out.println("--------------------");
         sum = 0;
-        for (int i=0; i < sales.length; i++)
-        {
-            System.out.println("\t " + i + "\t\t\t" + sales[i]);
+        for (int i = 0; i < sales.length; i++) {
+            System.out.println("\t " + (i + 1) + "\t\t\t" + sales[i]);
             sum += sales[i];
         }
 
         System.out.println("\nTotal sales: " + sum);
 
+        //Use your average method to print the average sales
+        System.out.println("Sales average: " + average(sales));
 
-        //TODO 1b) Use your average method to print the average sales
+        //Use your indexOfMax method to print the index of the best sales person
+        //and the sales amount for that sales person.
+        System.out.println("Best sales: Salesperson " + (indexOfMax(sales) + 1) +
+                " with " + sales[indexOfMax(sales)] + " sales.");
 
-
-        //TODO 2b) Use your indexOfMax method to print the index of the best sales person
-        //TODO 2b) and the sales amount for that sales person.
-
-
-        //TODO 3b) Do the same for the worst sales person
-
-
-
+        //Do the same for the worst sales person
+        System.out.println("Worst sales: Salesperson " + (indexOfMin(sales) + 1) +
+                " with " + sales[indexOfMin(sales)] + " sales.");
     }
 
     public static double average(int[] arr) throws IllegalStateException
     {
-        //TODO 1a) compute and return the average of arr
-        //Be sure to use the length instance variable and you may assume that the array is full.
-        //If the array does not have at least 1 element in it, throw an IllegalStateException
-
-        return 0;
+        //compute and return the average of arr
+        if (arr.length < 1) {
+            throw new IllegalStateException("arr length cannot be less than 1!");
+        }
+        int average = 0;
+        for (int x : arr) {
+            average += x;
+        }
+        return average / (double) arr.length;
     }
 
-    public static int indexOfMax(int[] arr)
-    {
-        //TODO 2a) find and return the index of the max value in arr
-
-        return 0;
+    public static int indexOfMax(int[] arr) {
+        //find and return the index of the max value in arr
+        int currentMax = arr[0];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (currentMax < arr[i]) {
+                currentMax = arr[i];
+                index = i;
+            }
+        }
+        return index;
     }
 
-    //TODO 3a) Do the same for an indexOfMin method
-
-
+    //Do the same for an indexOfMin method
+    public static int indexOfMin(int[] arr) {
+        int currentMin = arr[0];
+        int index = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (currentMin > arr[i]) {
+                currentMin = arr[i];
+                index = i;
+            }
+        }
+        return index;
+    }
 }
