925f78e21396096fb1faa5c37082aafcb8edfe66
Safyire <safyire.n8@gmail.com>
2018-10-26 08:51:19 -0700

    Updated CardGame

--- src/CardGame/Card.java
+++ src/CardGame/Card.java
@@ -30,4 +30,37 @@ public class Card {
     public String getSuit() {
         return suit;
     }
+
+    public int getValue() {
+        switch (face) {
+            case "Ace":
+                return 1;
+            case "Deuce":
+                return 2;
+            case "Three":
+                return 3;
+            case "Four":
+                return 4;
+            case "Five":
+                return 5;
+            case "Six":
+                return 6;
+            case "Seven":
+                return 7;
+            case "Eight":
+                return 8;
+            case "Nine":
+                return 9;
+            case "Ten":
+                return 10;
+            case "Jack":
+                return 10;
+            case "Queen":
+                return 10;
+            case "King":
+                return 10;
+            default:
+                return 0;
+        }
+    }
 }
--- src/CardGame/DeckOfCardsTest.java
+++ src/CardGame/DeckOfCardsTest.java
@@ -15,15 +15,28 @@ import java.util.ArrayList;
 public class DeckOfCardsTest {
     public static void main (String[] args) {
         DeckOfCards deck = new DeckOfCards();
-        ArrayList<Card> dealerHand = new ArrayList<>(2);
-        ArrayList<Card> playerHand = new ArrayList<>(2);
+        ArrayList<Card> dealerHand = new ArrayList<>();
+        ArrayList<Card> playerHand = new ArrayList<>();
+        int dealerHandValue = 0;
+        int playerHandValue = 0;
 
+        deck.shuffle();
+
+        //Initialize hands
         for (int i = 0; i < 2; i++) {
-            dealerHand.set(i, deck.dealCard());
-            playerHand.set(i, deck.dealCard());
+            dealerHand.add(i, deck.dealCard());
+            playerHand.add(i, deck.dealCard());
         }
 
-        System.out.println("Dealer's hand: " + dealerHand.get(0));
-        System.out.println("Player's hand: " + playerHand.get(0) + ", " + playerHand.get(1));
+        //Calculate total value of each hand
+        for (int i = 0; i < 2; i++) {
+            dealerHandValue += dealerHand.get(i).getValue();
+            playerHandValue += playerHand.get(i).getValue();
+        }
+
+        //Prints all current hands and value
+        System.out.println("Dealer's hand: " + dealerHand.get(0).getFace() + " (??)");
+        System.out.println("Player's hand: " + playerHand.get(0).getFace() + ", " +
+                playerHand.get(1).getFace() + " (" + playerHandValue + ")");
     }
 }
