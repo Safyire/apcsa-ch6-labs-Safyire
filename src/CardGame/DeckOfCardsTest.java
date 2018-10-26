aabd6fe8e675c63b4813e3f9254d4ec3dcd38153
Safyire <safyire.n8@gmail.com>
2018-10-26 08:34:29 -0700

    Updated CardGame

--- src/CardGame/DeckOfCardsTest.java
+++ src/CardGame/DeckOfCardsTest.java
@@ -10,27 +10,20 @@ package CardGame;
  * Tester program to shuffle and deal a deck of Card objects
  */
 
-public class DeckOfCardsTest
-{
+import java.util.ArrayList;
+
+public class DeckOfCardsTest {
     public static void main (String[] args) {
-        DeckOfCards myDeckOfCards = new DeckOfCards();
-        myDeckOfCards.shuffle(); // put Card objects in random order
+        DeckOfCards deck = new DeckOfCards();
+        ArrayList<Card> dealerHand = new ArrayList<>(2);
+        ArrayList<Card> playerHand = new ArrayList<>(2);
 
-        // -----------------------------------------------------------
-        //  print all 52 Cards in the order in which they are dealt
-        // -----------------------------------------------------------
-        for (int i = 0; i < 13; i++)
-        {
-            // printf method is used for formatting output
-            // print string (%) in a space of 20 characters (20s)
-            // the - in the (-20s) makes the String left align
-            // printf can be used with floats, ints and other types as well
-            // cheat sheet @ https://thejavacheatsheet.wordpress.com/2015/04/11/printf/
-            // Guide #2 https://www.cs.colostate.edu/~cs160/.Summer16/resources/Java_printf_method_quick_reference.pdf
-            System.out.printf("%-20s%-20s%-20s%-20s\n",
-                myDeckOfCards.dealCard(), myDeckOfCards.dealCard(),
-                myDeckOfCards.dealCard(), myDeckOfCards.dealCard());
-
+        for (int i = 0; i < 2; i++) {
+            dealerHand.set(i, deck.dealCard());
+            playerHand.set(i, deck.dealCard());
         }
+
+        System.out.println("Dealer's hand: " + dealerHand.get(0));
+        System.out.println("Player's hand: " + playerHand.get(0) + ", " + playerHand.get(1));
     }
 }
